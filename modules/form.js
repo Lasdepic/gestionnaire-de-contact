5dc1fb5c26ea2ca67aa77d00f2413b984c4ae882
Eszter112 <balieszter1212@gmai.com>
2025-07-30 14:43:07 +0200

    final

--- modules/form.js
+++ modules/form.js
@@ -1,24 +1,34 @@
-const container = document.createElement("div");
-document.body.appendChild(container);
+const divFormCreateUser = document.createElement("div");
+divFormCreateUser.classList.add("divFormCreateUser");
+document.body.appendChild(divFormCreateUser);
 
+// nom
 const nomLabel = document.createElement("label");
 nomLabel.textContent = "Nom";
-container.appendChild(nomLabel);
+divFormCreateUser.appendChild(nomLabel);
 const nomInput = document.createElement("input");
-container.appendChild(nomInput);
+divFormCreateUser.appendChild(nomInput);
 
+// prenom
 const prenomLabel = document.createElement("label");
 prenomLabel.textContent = "Prénom";
-container.appendChild(prenomLabel);
+divFormCreateUser.appendChild(prenomLabel);
 const prenomInput = document.createElement("input");
-container.appendChild(prenomInput);
+divFormCreateUser.appendChild(prenomInput);
 
+// numero
 const numeroLabel = document.createElement("label");
 numeroLabel.textContent = "Numéro";
-container.appendChild(numeroLabel);
+numeroLabel.type = "number";
+divFormCreateUser.appendChild(numeroLabel);
 const numeroInput = document.createElement("input");
-container.appendChild(numeroInput);
+numeroInput.placeholder = "Entrez votre numero de telephone";
 
+numeroInput.setAttribute("type", "number");
+divFormCreateUser.appendChild(numeroInput);
+
+// buttonValidation
 const boutonValidation = document.createElement("button");
+boutonValidation.classList.add("boutonValidation");
 boutonValidation.textContent = "Valider";
-container.appendChild(boutonValidation);
+divFormCreateUser.appendChild(boutonValidation);
